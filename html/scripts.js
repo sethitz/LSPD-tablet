81c7589ff3408a329f0335fb0015b7b82ed286e9
Seth <106752987+sethitz@users.noreply.github.com>
2022-06-05 11:31:07 -0500

    Add files via upload

--- html/scripts.js
+++ html/scripts.js
@@ -14,7 +14,7 @@ var ex_callout_button = document.getElementById("ex-callout"),
     ex_callout_content = document.getElementById("ex-callout-content"),
     callout_active = null;
 
-function option1_change(){
+function option_change(option_changin, container_changin,){
     if(option_active != null){
         option_active.classList.add("option");
         option_active.classList.remove("option-active");
@@ -23,89 +23,33 @@ function option1_change(){
     container_active.classList.remove("option-container-active");
     container_active.classList.add("option-container");
 
-    option1.classList.remove("option");
-    option1.classList.add("option-active");
+    option_changin.classList.remove("option");
+    option_changin.classList.add("option-active");
 
-    container1.classList.remove("option-container");
-    container1.classList.add("option-container-active");
+    container_changin.classList.remove("option-container");
+    container_changin.classList.add("option-container-active");
 
-    option_active = option1;
-    container_active = container1;
+    option_active = option_changin;
+    container_active = container_changin;  
 };
 
-function option2_change(){
-    if(option_active != null){
-        option_active.classList.add("option");
-        option_active.classList.remove("option-active");
-    };
-
-    container_active.classList.remove("option-container-active");
-    container_active.classList.add("option-container");
-
-    option2.classList.remove("option");
-    option2.classList.add("option-active");
-
-    container2.classList.remove("option-container");
-    container2.classList.add("option-container-active");
-
-    option_active = option2;
-    container_active = container2;
-};
-
-function option3_change(){
-    if(option_active != null){
-        option_active.classList.add("option");
-        option_active.classList.remove("option-active");
-    };
-
-    container_active.classList.remove("option-container-active");
-    container_active.classList.add("option-container");
-
-    option3.classList.remove("option");
-    option3.classList.add("option-active");
-
-    container3.classList.remove("option-container");
-    container3.classList.add("option-container-active");
-
-    option_active = option3;
-    container_active = container3;
-};
-
-function option4_change(){
-    if(option_active != null){
-        option_active.classList.add("option");
-        option_active.classList.remove("option-active");
-    };
-
-    container_active.classList.remove("option-container-active");
-    container_active.classList.add("option-container");
-
-    option4.classList.remove("option");
-    option4.classList.add("option-active");
-
-    container4.classList.remove("option-container");
-    container4.classList.add("option-container-active");
-
-    option_active = option4;
-    container_active = container4;
-};
-
-function ex_callout_show(){
+function callout_show(callout_button_changin, callout_content_changin){
     if (callout_active != null){
-        callout_active.classList.remove("callout-active")
-        callout_active.classList.add("callout")
-    }
-    ex_callout_button.classList.remove("callout")
-    ex_callout_button.classList.add("callout-active")
+        callout_active.classList.remove("callout-active");
+        callout_active.classList.add("callout");
+    };
+    callout_button_changin.classList.remove("callout");
+    callout_button_changin.classList.add("callout-active");
 
-    ex_callout_content.classList.remove("callout-content")
-    ex_callout_content.classList.add("callout-content-active")
+    callout_content_changin.classList.remove("callout-content");
+    callout_content_changin.classList.add("callout-content-active");
 
-    callout_active = ex_callout_button;
-}
+    callout_active = callout_button_changin;
+};
 
-option1.addEventListener("click", option1_change,null);
-option2.addEventListener("click", option2_change,null);
-option3.addEventListener("click", option3_change,null);
-option4.addEventListener("click", option4_change,null);
-ex_callout_button.addEventListener("click", ex_callout_show, null)
+option1.addEventListener("click", function(){option_change(option1, container1);}, null);
+option2.addEventListener("click", function(){option_change(option2, container2);}, null);
+option3.addEventListener("click", function(){option_change(option3, container3);}, null);
+option4.addEventListener("click", function(){option_change(option4, container4);}, null);
+
+ex_callout_button.addEventListener("click", function(){ callout_show(ex_callout_button, ex_callout_content);}, null)
